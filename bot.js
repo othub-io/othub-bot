c8b6482086385dc5183150b8455357d15528a50d
CosmiCloud <tybranha@gmail.com>
2023-02-17 09:49:15 -0500

    first commit

--- bot.js
+++ bot.js
@@ -293,10 +293,10 @@ cron.schedule(process.env.ASK_MONITOR, async function () {
           .run(0, cur_member.member_id.tg_id)
       }
     } else {
-      tg_member = await bot.telegram.getChatMember(
-        process.env.GROUP,
-        cur_member.member_id.tg_id
-      )
+      //   tg_member = await bot.telegram.getChatMember(
+      //     process.env.GROUP,
+      //     cur_member.member_id.tg_id
+      //   )
     }
 
     console.log(`TG MEMBER: ` + tg_member)
