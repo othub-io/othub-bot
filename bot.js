ff248a9cc82ee4241d6fbcee1f3bd32ff182811d
CosmiCloud <tybranha@gmail.com>
2023-02-16 22:54:02 -0500

    first commit

--- bot.js
+++ bot.js
@@ -273,64 +273,51 @@ cron.schedule(process.env.ASK_MONITOR, async function () {
 
     console.log(`NONCOMPLAINT: ` + JSON.stringify(noncompliant))
 
-    if (noncompliant != '') {
-      for (b = 0; b < noncompliant.length; ++b) {
-        node_id = JSON.stringify(noncompliant[b])
-
-        warnings = await bot_db
-          .prepare(
-            'SELECT warnings FROM node_compliance WHERE tg_id = ? AND node_id = ?'
-          )
-          .all(cur_member.member_id, node_id)
-
-        if (warnings == '') {
-          warnings = 0
-        }
-
-        if (warnings == '6') {
-          await bot.telegram.sendMessage(
-            cur_member.member_id,
-            `Node ${node_id} is being kicked from the Alliance for not adhering to the ask range. Please reverify and stay within the ask range.`
-          )
-
-          await alliance_db
-            .prepare(`DELETE FROM member_nodes node_id = ? COLLATE NOCASE`)
-            .run(node_id)
-        } else {
-          await bot_db
-            .prepare(`REPLACE INTO node_compliance VALUES (?,?,?,?)`)
-            .run(node_id, member_id, 'out_of_range', warnings + 1)
-
-          await bot.telegram.sendMessage(
-            cur_member.member_id,
-            `Node ${node_id} is outside of the Alliance ask range. You have ${
-              7 - (warnings + 1)
-            } days to comply before being kicked.`
-          )
-        }
-      }
-    } else {
+    if (noncompliant == '') {
       console.log(`Clearing warnings.`)
-      await alliance_db
-        .prepare(`UPDATE node_compliance SET warnings = ? WHERE tg_id = ?`)
-        .run(0, member.tg_id)
+      //   await alliance_db
+      //     .prepare(
+      //       `INSERT INTO node_compliance (node_id,tg_id,type,warnings) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE  SET warnings = ? WHERE tg_id = ?`
+      //     )
+      //     .run(0, member.tg_id, 'out_of_range')
+      return
     }
 
-    exists = await alliance_db
-      .prepare('SELECT * FROM member_nodes WHERE tg_id = ?')
-      .all(cur_member.member_id)
+    for (b = 0; b < noncompliant.length; ++b) {
+      node_id = JSON.stringify(noncompliant[b])
 
-    console.log(`Exists: ${exists}`)
+      warnings = await bot_db
+        .prepare(
+          'SELECT warnings FROM node_compliance WHERE tg_id = ? AND node_id = ?'
+        )
+        .all(cur_member.member_id, node_id)
 
-    if (exists == '') {
+      if (warnings == '') {
+        warnings = 0
+      }
+
+      if (warnings != '6') {
+        await bot_db
+          .prepare(`REPLACE INTO node_compliance VALUES (?,?,?,?)`)
+          .run(node_id, member_id, 'out_of_range', warnings + 1)
+
+        await bot.telegram.sendMessage(
+          cur_member.member_id,
+          `Node ${node_id} is outside of the Alliance ask range. You have ${
+            7 - (warnings + 1)
+          } days to comply before being kicked.`
+        )
+        return
+      }
+
       await bot.telegram.sendMessage(
         cur_member.member_id,
-        `You have no registered nodes with the Alliance. Please verify a node to become a member again.`
+        `Node ${node_id} is being kicked from the Alliance for not adhering to the ask range. Please reverify and stay within the ask range.`
       )
 
-      bot.telegram.banChatMember(cur_member.member_id)
-      bot.telegram.unbanChatMember(cur_member.member_id)
-      return
+      await alliance_db
+        .prepare(`DELETE FROM member_nodes node_id = ? COLLATE NOCASE`)
+        .run(node_id)
     }
 
     console.log(`END`)
