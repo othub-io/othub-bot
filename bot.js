61f9a614b96f89fad817c3e639d6e85f6e517b06
CosmiCloud <tybranha@gmail.com>
2023-02-17 11:10:05 -0500

    first commit

--- bot.js
+++ bot.js
@@ -289,7 +289,7 @@ cron.schedule(process.env.ASK_MONITOR, async function () {
     }
 
     console.log(`PREVIOUS OFFENDER: ` + previous_offender)
-    if (previous_offender != '') {
+    if (previous_offender != 'no') {
       console.log(`updating warning back to 0`)
       await bot_db
         .prepare(`UPDATE node_compliance (warnings) VALUES (?) WHERE tg_id = ?`)
