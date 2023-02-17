67e48b86db19708cd2decf143291907144aeb638
CosmiCloud <tybranha@gmail.com>
2023-02-17 11:03:56 -0500

    first commit

--- bot.js
+++ bot.js
@@ -298,7 +298,7 @@ cron.schedule(process.env.ASK_MONITOR, async function () {
 
     tg_member = await bot.telegram.getChatMember(
       process.env.GROUP,
-      cur_member.member_id.tg_id
+      JSON.stringify(cur_member.member_id.tg_id)
     )
 
     console.log(`TG MEMBER: ` + tg_member)
