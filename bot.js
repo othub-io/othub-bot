abbcc247bad74d70ec0e637def1fdf33508627ed
CosmiCloud <tybranha@gmail.com>
2023-03-11 15:41:31 -0500

    commit

--- bot.js
+++ bot.js
@@ -501,7 +501,7 @@ cron.schedule(process.env.HOURLY, async function () {
     })
     .catch(error => console.log(`Error : ${error}`))
 
-  console.log(`RESULT: ` + JSON.stringify(result))
+  console.log(JSON.stringify(result))
   trac_committed = result.data.data[0].balance
   trac_committed = (trac_committed / 1000000000000)
     .toFixed(3)
