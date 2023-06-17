5aec08a93230f305b5c0114f16f1fdeb653de008
valcyclovir <swaggrenox@gmail.com>
2023-06-17 11:41:47 -0400

    added ctx deletemessage

--- src/modules/networkPubs.js
+++ src/modules/networkPubs.js
@@ -37,51 +37,58 @@ function getLast24HourStats() {
 
 // Function to fetch and send hourly pubs data
 async function fetchAndSendHourlyPubs(ctx) {
-  try {
-    // Retrieve the last row from the v_pubs_stats_last1h table
-    const lastHourStats = await getLastHourStats();
-
-    // Extract the desired fields from the last row
-    const totalPubs = lastHourStats.totalPubs;
-    const totalTracSpent = parseInt(lastHourStats.totalTracSpent);
-    const avgPubPrice = parseFloat(lastHourStats.avgPubPrice).toFixed(2);
-    const avgBid = parseFloat(lastHourStats.avgBid).toFixed(3);
-    const avgEpochs = parseInt(lastHourStats.avgEpochsNumber);
-
-    // Generate the formatted message
-    const message = `Total pubs: ${totalPubs}\nTRAC spent: ${totalTracSpent}\nPub price: ${avgPubPrice}\nBid: ${avgBid}\nEpochs: ${avgEpochs}`;
-
-    // Send the message as a reply
-    ctx.reply(message);
-  } catch (error) {
-    console.error('An error occurred:', error);
-    ctx.reply('An error occurred while retrieving hourly pubs statistics.');
+    try {
+      // Delete the command message
+      await ctx.deleteMessage();
+  
+      // Retrieve the last row from the v_pubs_stats_last1h table
+      const lastHourStats = await getLastHourStats();
+  
+      // Extract the desired fields from the last row
+      const totalPubs = lastHourStats.totalPubs;
+      const totalTracSpent = parseInt(lastHourStats.totalTracSpent);
+      const avgPubPrice = parseFloat(lastHourStats.avgPubPrice).toFixed(2);
+      const avgBid = parseFloat(lastHourStats.avgBid).toFixed(3);
+      const avgEpochs = parseInt(lastHourStats.avgEpochsNumber);
+  
+      // Generate the formatted message
+      const message = `Total pubs: ${totalPubs}\nTRAC spent: ${totalTracSpent}\nPub price: ${avgPubPrice}\nBid: ${avgBid}\nEpochs: ${avgEpochs}`;
+  
+      // Send the message as a reply
+      ctx.reply(message);
+    } catch (error) {
+      console.error('An error occurred:', error);
+      ctx.reply('An error occurred while retrieving hourly pubs statistics.');
+    }
   }
-}
-
-// Function to fetch and send daily pubs data
-async function fetchAndSendDailyPubs(ctx) {
-  try {
-    // Retrieve the last row from the v_pubs_stats_last24h table
-    const last24HourStats = await getLast24HourStats();
-
-    // Extract the desired fields from the last row
-    const totalPubs = last24HourStats.totalPubs;
-    const totalTracSpent = parseInt(last24HourStats.totalTracSpent);
-    const avgPubPrice = parseFloat(last24HourStats.avgPubPrice).toFixed(2);
-    const avgBid = parseFloat(last24HourStats.avgBid).toFixed(3);
-    const avgEpochs = parseInt(last24HourStats.avgEpochsNumber);
-
-    // Generate the formatted message
-    const message = `Total pubs: ${totalPubs}\nTRAC spent: ${totalTracSpent}\nPub price: ${avgPubPrice}\nBid: ${avgBid}\nEpochs: ${avgEpochs}`;
-
-    // Send the message as a reply
-    ctx.reply(message);
-  } catch (error) {
-    console.error('An error occurred:', error);
-    ctx.reply('An error occurred while retrieving daily pubs statistics.');
+  
+  // Function to fetch and send daily pubs data
+  async function fetchAndSendDailyPubs(ctx) {
+    try {
+      // Delete the command message
+      await ctx.deleteMessage();
+  
+      // Retrieve the last row from the v_pubs_stats_last24h table
+      const last24HourStats = await getLast24HourStats();
+  
+      // Extract the desired fields from the last row
+      const totalPubs = last24HourStats.totalPubs;
+      const totalTracSpent = parseInt(last24HourStats.totalTracSpent);
+      const avgPubPrice = parseFloat(last24HourStats.avgPubPrice).toFixed(2);
+      const avgBid = parseFloat(last24HourStats.avgBid).toFixed(3);
+      const avgEpochs = parseInt(last24HourStats.avgEpochsNumber);
+  
+      // Generate the formatted message
+      const message = `Total pubs: ${totalPubs}\nTRAC spent: ${totalTracSpent}\nPub price: ${avgPubPrice}\nBid: ${avgBid}\nEpochs: ${avgEpochs}`;
+  
+      // Send the message as a reply
+      ctx.reply(message);
+    } catch (error) {
+      console.error('An error occurred:', error);
+      ctx.reply('An error occurred while retrieving daily pubs statistics.');
+    }
   }
-}
+  
 
 module.exports = {
   fetchAndSendHourlyPubs,
