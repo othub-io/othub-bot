e7174f42e7b90cc77109eaed4742c65ccda2c639
valcyclovir <swaggrenox@gmail.com>
2023-06-17 23:06:13 -0400

    bug fix

--- src/modules/systemCommands.js
+++ src/modules/systemCommands.js
@@ -8,40 +8,40 @@ function isAdmin(ctx) {
 }
 
 const commands = {
-  'otnode-restart': 'systemctl restart otnode.service',
-  'otnode-stop': 'systemctl stop otnode.service',
-  'otnode-start': 'systemctl start otnode.service',
-  'otnode-logs': 'journalctl -u otnode --output cat -n 100',
+  'otnoderestart': 'systemctl restart otnode.service',
+  'otnodestop': 'systemctl stop otnode.service',
+  'otnodestart': 'systemctl start otnode.service',
+  'otnodelogs': 'journalctl -u otnode --output cat -n 100',
 
-  'otnode-restart2': 'systemctl restart otnode2.service',
-  'otnode-stop2': 'systemctl stop otnode2.service',
-  'otnode-start2': 'systemctl start otnode2.service',
-  'otnode-logs2': 'journalctl -u otnode2 --output cat -n 100',
+  'otnoderestart2': 'systemctl restart otnode2.service',
+  'otnodestop2': 'systemctl stop otnode2.service',
+  'otnodestart2': 'systemctl start otnode2.service',
+  'otnodelogs2': 'journalctl -u otnode2 --output cat -n 100',
 
-  'othub-bot-restart': 'systemctl restart othub-bot',
-  'othub-bot-stop': 'systemctl stop othub-bot',
-  'othub-bot-start': 'systemctl start othub-bot',
-  'othub-bot-logs': 'journalctl -u othub-bot --output cat -n 100',
+  'othubbotrestart': 'systemctl restart othub-bot',
+  'othubbotstop': 'systemctl stop othub-bot',
+  'othubbotstart': 'systemctl start othub-bot',
+  'othubbotlogs': 'journalctl -u othub-bot --output cat -n 100',
 
-  'otp-sync-restart': 'systemctl restart otp-sync',
-  'otp-sync-stop': 'systemctl stop otp-sync',
-  'otp-sync-start': 'systemctl start otp-sync',
-  'otp-sync-logs': 'journalctl -u otp-sync --output cat -n 100',
+  'otpsyncrestart': 'systemctl restart otp-sync',
+  'otpsyncstop': 'systemctl stop otp-sync',
+  'otpsyncstart': 'systemctl start otp-sync',
+  'otpsynclogs': 'journalctl -u otp-sync --output cat -n 100',
 
-  'otp-sync2-restart': 'systemctl restart otp-sync2',
-  'otp-sync2-stop': 'systemctl stop otp-sync2',
-  'otp-sync2-start': 'systemctl start otp-sync2',
-  'otp-sync2-logs': 'journalctl -u otp-sync2 --output cat -n 100',
+  'otpsync2restart': 'systemctl restart otp-sync2',
+  'otpsync2stop': 'systemctl stop otp-sync2',
+  'otpsync2start': 'systemctl start otp-sync2',
+  'otpsync2logs': 'journalctl -u otp-sync2 --output cat -n 100',
 
-  'otnode-api-estart': 'systemctl restart otnode-api',
-  'otnode-api-top': 'systemctl stop otnode-api',
-  'otnode-api-tart': 'systemctl start otnode-api',
-  'otnode-api-ogs': 'journalctl -u otnode-api --output cat -n 100',
+  'otnodeapirestart': 'systemctl restart otnode-api',
+  'otnodeapistop': 'systemctl stop otnode-api',
+  'otnodeapistart': 'systemctl start otnode-api',
+  'otnodeapilogs': 'journalctl -u otnode-api --output cat -n 100',
 
-  'otnode-app-restart': 'systemctl restart otnode-app',
-  'otnode-app-stop': 'systemctl stop otnode-app',
-  'otnode-app-start': 'systemctl start otnode-app',
-  'otnode-app-logs': 'journalctl -u otnode-app --output cat -n 100',
+  'otnodeapprestart': 'systemctl restart otnode-app',
+  'otnodeappstop': 'systemctl stop otnode-app',
+  'otnodeappstart': 'systemctl start otnode-app',
+  'otnodeapplogs': 'journalctl -u otnode-app --output cat -n 100',
 };
 
 async function commandsHandler(bot) {
