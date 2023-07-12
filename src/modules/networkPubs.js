3f9b119bf68570fbd83d716e874b72da1c8e03c9
valcyclovir <swaggrenox@gmail.com>
2023-07-12 18:20:18 -0400

    added emojis

--- src/modules/networkPubs.js
+++ src/modules/networkPubs.js
@@ -74,7 +74,16 @@ async function fetchAndSendHourlyPubs(ctx) {
     const avgBid = parseFloat(lastHourStats.avgBid).toFixed(2);
     const avgEpochs = parseInt(lastHourStats.avgEpochsNumber);
 
-    const message = `Last Hour Stats\nTotal pubs: ${totalPubs}\nTRAC spent: ${totalTracSpent}\nPub price: ${avgPubPrice}\nBid: ${avgBid}\nEpochs: ${avgEpochs}`;
+    let totalPubsEmoji = totalPubs > 100 ? '🚀' : totalPubs >= 75 ? '✈️' : totalPubs >= 50 ? '🎈' : totalPubs >= 25 ? '💭' : '☠️';
+    let totalTracSpentEmoji = totalTracSpent > 400 ? '🤑' : totalTracSpent >= 300 ? '💰' : totalTracSpent >= 200 ? '💸' : totalTracSpent >= 100 ? '💵' : '🪙';
+    let avgPubPriceEmoji = avgPubPrice > 5 ? '🤮' : avgPubPrice >= 4 ? '🤕' : avgPubPrice >= 3 ? '☹️' : avgPubPrice >= 2 ? '🙂' : '😃';
+
+    const message = `== Last Hour \u{1F4CA} ==
+${totalPubsEmoji}Total pubs: ${totalPubs}
+${totalTracSpentEmoji}TRAC spent: ${totalTracSpent}
+${avgPubPriceEmoji}Pub price: ${avgPubPrice}
+⚖️Bid: ${avgBid}
+📏Epochs: ${avgEpochs}`;
 
     return await ctx.reply(message);
   } catch (error) {
@@ -102,7 +111,16 @@ async function fetchAndSendDailyPubs(ctx) {
     const avgBid = parseFloat(last24HourStats.avgBid).toFixed(2);
     const avgEpochs = parseInt(last24HourStats.avgEpochsNumber);
 
-    const message = `Last Day Stats\nTotal pubs: ${totalPubs}\nTRAC spent: ${totalTracSpent}\nPub price: ${avgPubPrice}\nBid: ${avgBid}\nEpochs: ${avgEpochs}`;
+    let totalPubsEmoji = totalPubs > 2400 ? '🚀' : totalPubs >= 1800 ? '✈️' : totalPubs >= 1200 ? '🎈' : totalPubs >= 600 ? '💭' : '☠️';
+    let totalTracSpentEmoji = totalTracSpent > 9600 ? '🤑' : totalTracSpent >= 7200 ? '💰' : totalTracSpent >= 4800 ? '💸' : totalTracSpent >= 2400 ? '💵' : '🪙';
+    let avgPubPriceEmoji = avgPubPrice > 5 ? '🤮' : avgPubPrice >= 4 ? '🤕' : avgPubPrice >= 3 ? '☹️' : avgPubPrice >= 2 ? '🙂' : '😃';
+
+    const message = `== Last Day \u{1F4CA} ==
+${totalPubsEmoji}Total pubs: ${totalPubs}
+${totalTracSpentEmoji}TRAC spent: ${totalTracSpent}
+${avgPubPriceEmoji}Pub price: ${avgPubPrice}
+⚖️Bid: ${avgBid}
+📏Epochs: ${avgEpochs}`;
 
     return await ctx.reply(message);
   } catch (error) {
@@ -139,7 +157,16 @@ async function fetchAndSendWeeklyPubs(ctx) {
     const avgBid = parseFloat(lastWeekStats.avgBid).toFixed(2);
     const avgEpochs = parseInt(lastWeekStats.avgEpochsNumber);
 
-    const message = `Last 7 Days Stats\nTotal pubs: ${totalPubs}\nTRAC spent: ${totalTracSpent}\nPub price: ${avgPubPrice}\nBid: ${avgBid}\nEpochs: ${avgEpochs}`;
+    let totalPubsEmoji = totalPubs > 16800 ? '🚀' : totalPubs >= 12600 ? '✈️' : totalPubs >= 8400 ? '🎈' : totalPubs >= 4200 ? '💭' : '☠️';
+    let totalTracSpentEmoji = totalTracSpent > 67200 ? '🤑' : totalTracSpent >= 50400 ? '💰' : totalTracSpent >= 33600 ? '💸' : totalTracSpent >= 16800 ? '💵' : '🪙';
+    let avgPubPriceEmoji = avgPubPrice > 5 ? '🤮' : avgPubPrice >= 4 ? '🤕' : avgPubPrice >= 3 ? '☹️' : avgPubPrice >= 2 ? '🙂' : '😃';
+
+    const message = `== Last Week \u{1F4CA} ==
+${totalPubsEmoji}Total pubs: ${totalPubs}
+${totalTracSpentEmoji}TRAC spent: ${totalTracSpent}
+${avgPubPriceEmoji}Pub price: ${avgPubPrice}
+⚖️Bid: ${avgBid}
+📏Epochs: ${avgEpochs}`;
 
     return await ctx.reply(message);
   } catch (error) {
@@ -176,7 +203,16 @@ async function fetchAndSendMonthlyPubs(ctx) {
     const avgBid = parseFloat(lastMonthStats.avgBid).toFixed(2);
     const avgEpochs = parseInt(lastMonthStats.avgEpochsNumber);
 
-    const message = `Last 30 Days Stats\nTotal pubs: ${totalPubs}\nTRAC spent: ${totalTracSpent}\nPub price: ${avgPubPrice}\nBid: ${avgBid}\nEpochs: ${avgEpochs}`;
+    let totalPubsEmoji = totalPubs > 72000 ? '🚀' : totalPubs >= 54000 ? '✈️' : totalPubs >= 36000 ? '🎈' : totalPubs >= 18000 ? '💭' : '☠️';
+    let totalTracSpentEmoji = totalTracSpent > 288000 ? '🤑' : totalTracSpent >= 216000 ? '💰' : totalTracSpent >= 144000 ? '💸' : totalTracSpent >= 72000 ? '💵' : '🪙';
+    let avgPubPriceEmoji = avgPubPrice > 5 ? '🤮' : avgPubPrice >= 4 ? '🤕' : avgPubPrice >= 3 ? '☹️' : avgPubPrice >= 2 ? '🙂' : '😃';
+
+    const message = `== Last Month \u{1F4CA} ==
+${totalPubsEmoji}Total pubs: ${totalPubs}
+${totalTracSpentEmoji}TRAC spent: ${totalTracSpent}
+${avgPubPriceEmoji}Pub price: ${avgPubPrice}
+⚖️Bid: ${avgBid}
+📏Epochs: ${avgEpochs}`;
 
     return await ctx.reply(message);
   } catch (error) {
