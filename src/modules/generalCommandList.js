dc4b015d4a202090eba467b0a50f504e8970cfe4
valcyclovir <swaggrenox@gmail.com>
2023-07-29 00:29:21 -0400

    update general command list

--- src/modules/generalCommandList.js
+++ src/modules/generalCommandList.js
@@ -1,4 +1,5 @@
 module.exports = {
+    'publish': 'Publish Knowledge Assets on the DKG',
     'hourlypubs': 'Show network stats for the last hour',
     'dailypubs': 'Show network stats for the last day',
     'weeklypubs': 'Show network stats for the last week',
