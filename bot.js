b3ca8c37c10946c6cb47929a9ec0aae5e0cb220b
othub-io <othub@protonmail.com>
2024-09-28 00:15:15 +0000

    improved daily message

--- bot.js
+++ bot.js
@@ -97,17 +97,38 @@ cron.schedule(process.env.HOURLY, async () => {
 });
 
 cron.schedule(process.env.DAILY, async () => {
-  console.log('Running daily publication stats...');
-  const message = await autoTweet.postStatistics('daily');
-  if (message) {
-    await autoTweet.postTweet(message);
-    await postTelegramMessage(message, process.env.OTC_ID, process.env.OTC_THREAD_ID);
-  } else {
-    console.error('Failed to generate daily statistics message.');
+  try {
+    console.log('Running daily publication stats...');
+    const message = await autoTweet.gatherAndDisplayChainStatsWithImage();
+    if (message) {
+      await autoTweet.postTweet(message);
+      await postTelegramMessage(message, process.env.OTC_ID, process.env.OTC_THREAD_ID);
+    } else {
+      console.error('Failed to generate daily statistics message.');
+    }
+  } catch (error) {
+    console.error('Error in daily publication stats:', error);
   }
 }, {
   timezone: 'America/New_York'
 });
+
+// (async () => {
+//   console.log('Manually testing the daily publication stats...');
+//   try {
+//     const message = await autoTweet.gatherAndDisplayChainStatsWithImage();
+//     if (message) {
+//       console.log('Tweet posted:', message);
+//       await postTelegramMessage(message, process.env.OTHUB_ADMIN_ID);
+//     } else {
+//       console.error('Failed to generate daily statistics message.');
+//     }
+//   } catch (error) {
+//     console.error('Error in manual tweet posting:', error);
+//   }
+// }, {
+//   timezone: 'America/New_York'
+// })();
 
 cron.schedule(process.env.WEEKLY, async () => {
   console.log('Running weekly publication stats...');
--- src/modules/autoTweet.js
+++ src/modules/autoTweet.js
@@ -2,33 +2,67 @@ require('dotenv').config();
 const { TwitterApi } = require('twitter-api-v2');
 const mysql = require('mysql');
 const { getCoinPrice, getCoinCap, getCoinVolume } = require('./getCoinPrice.js');
+const { fetchDateTotalPubs, KnowledgeAssetsOverTime } = require('./networkStats.js');
+const { Readable } = require('stream');
+
+function bufferToStream(buffer) {
+  const stream = new Readable({
+    read() {
+      this.push(buffer);
+      this.push(null);
+    }
+  });
+   
+  return stream;
+}
+
+const pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : process.env.DBHOST,
+  user            : process.env.DBUSER,
+  password        : process.env.DBPASSWORD,
+  database        : process.env.DKG_DB
+});
 
 const connection = mysql.createConnection({
-    host: process.env.DBHOST,
-    user: process.env.DBUSER,
-    password: process.env.DBPASSWORD,
-    database: process.env.DKG_DB,
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DKG_DB,
 });
+
+function switchDatabase(dbName) {
+return new Promise((resolve, reject) => {
+    pool.config.connectionConfig.database = dbName;
+    resolve();
+});
+}
 
 function query(sql, args) {
   return new Promise((resolve, reject) => {
-    connection.query(sql, args, (error, rows) => {
-      if (error) return reject(error);
-      resolve(rows);
-    });
+      pool.getConnection((err, connection) => {
+          if (err) {
+              reject(err);
+          } else {
+              connection.query(sql, args, (error, results) => {
+                  // Always release the connection to the pool after use
+                  connection.release();
+                  if (error) reject(error);
+                  else resolve(results);
+              });
+          }
+      });
   });
 }
 
-const client = new TwitterApi({
-    appKey: process.env.TWITTER_CONSUMER_KEY,
-    appSecret: process.env.TWITTER_CONSUMER_SECRET,
-    accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  });
-  
-const bearer = new TwitterApi(process.env.TWITTER_BEARER_TOKEN);
+const twitterClient = new TwitterApi({
+  appKey: process.env.TWITTER_CONSUMER_KEY,
+  appSecret: process.env.TWITTER_CONSUMER_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
-const twitterClient = client.readWrite;
+const rwClient = twitterClient.readWrite;
 
 async function postTweet(message) {
     try {
@@ -52,11 +86,11 @@ function getRecordStats() {
   }
   
 async function fetchAndSendRecordStats(ctx) {
-const recordStats = await getRecordStats();
+  await switchDatabase(process.env.DKG_DB);
+  const recordStats = await getRecordStats();
 
 let message = "== DKG Record Statistics \u{1F3C6} ==\n\n";
 
-// Create an object to store the formatted records
 let formattedRecords = {
     'Top TRAC spent (hour)': '',
     'Top TRAC spent (day)': '',
@@ -95,6 +129,7 @@ await ctx.reply(message);
 }
 
 async function fetchNetworkStatistics() {
+  await switchDatabase(process.env.DKG_DB);
   const pubStats = await query('SELECT * FROM v_pubs_stats_total;');
   const nodeStats = await query('SELECT ROUND(AVG(nodeAsk), 3) AS avgNodeAsk, SUM(nodeStake) AS totalNodeStake, COUNT(*) AS totalNodes FROM v_nodes WHERE nodeStake >= 50000');
   const symbol = 'TRAC';
@@ -122,6 +157,7 @@ async function fetchNetworkStatistics() {
 }
 
 async function fetchPeriodStats(period) {
+  await switchDatabase(process.env.DKG_DB);
   let sql;
   switch (period) {
       case 'daily':
@@ -137,11 +173,19 @@ async function fetchPeriodStats(period) {
           throw new Error('Invalid period');
   }
 
-  const results = await query(sql);
-  return results[0];
+  return new Promise((resolve, reject) => {
+      connection.query(sql, (error, results) => {
+          if (error) {
+              reject(error);
+          } else {
+              resolve(results[0]);
+          }
+      });
+  });
 }
 
 async function postStatistics(period) {
+  await switchDatabase(process.env.DKG_DB);
   try {
       const networkStats = await fetchNetworkStatistics();
 
@@ -205,11 +249,13 @@ async function postStatistics(period) {
 }
 
 async function postNetworkStatistics() {
+  await switchDatabase(process.env.DKG_DB);
   const pubStats = await query('SELECT * FROM v_pubs_stats_total;');
   const nodeStats = await query('SELECT ROUND(AVG(nodeAsk), 3) AS avgNodeAsk, SUM(nodeStake) AS totalNodeStake, COUNT(*) AS totalNodes FROM v_nodes WHERE nodeStake >= 50000');
-  const dailyStats = await query('SELECT totalTracSpent FROM v_pubs_stats_last24h');
+  const dailyStats = await query('SELECT * FROM v_pubs_stats_last24h');
   
   const dailyTracSpent = Number(dailyStats[0].totalTracSpent);
+  const avgEpochsNumber = Number(dailyStats[0].avgEpochsNumber);
   const totalTracSpent = Number(pubStats[0].totalTracSpent);
   const totalNodes = Number(nodeStats[0].totalNodes);
   const totalNodeStake = Number(nodeStats[0].totalNodeStake);
@@ -258,16 +304,12 @@ async function postNetworkStatistics() {
   const marketCapFormatted = formatCurrency(marketCap);
   const volumeFormatted = formatCurrency(volume);
   
-  const message = `== Network Stats 📊 ==
-  💻Active nodes: ${totalNodesFormatted}
-  🥩TVL: ${tvlFormatted} ($${tvlUsdFormatted})
-  💵TRAC spent 24H: ${dailyTracSpentFormatted} ($${dailyTracSpentUsdFormatted})
-  💰TRAC spent total: ${totalTracSpentFormatted} ($${totalTracSpentUsdFormatted})
-  ⚖️Mcap: $${marketCapFormatted} | Volume: $${volumeFormatted}`;
-  
   return {
+      avgEpochsNumber,
+      dailyTracSpent,
       dailyTracSpentFormatted,
       dailyTracSpentUsdFormatted,
+      totalTracSpent,
       totalTracSpentFormatted,
       totalTracSpentUsdFormatted,
       tvlFormatted,
@@ -277,8 +319,166 @@ async function postNetworkStatistics() {
       volumeFormatted
   };
   }
-  
+
+const chainNameMap = {
+    [process.env.DKG_DB]: "🌐 Overall Statistics 🌐",
+    [process.env.OTP_DB]: "⬛️ NeuroWeb AI Parachain ⬛️",
+    [process.env.GNO_DB]: "🟩 Gnosis Chain 🟩",
+    [process.env.BASE_DB]: "🟦 Base Chain 🟦"
+};
+ 
+async function gatherAndDisplayChainStatsWithImage() {
+  const message = await gatherAndDisplayChainStats();
+
+  if (!message) return null;
+
+  try {
+    const data = await fetchDateTotalPubs();
+    const dates = data.map(row => new Date(row.date));
+    const totalPubsValues = data.map(row => row.totalPubs);
+    const imageBuffer = await KnowledgeAssetsOverTime(dates, totalPubsValues);
+
+    // Upload the media to Twitter
+    const mediaId = await rwClient.v1.uploadMedia(imageBuffer, { mimeType: 'image/png' });
+    // Post the tweet with media
+    const tweet = await rwClient.v2.tweet({
+      text: message,
+      media: { media_ids: [mediaId] }
+    });
+
+    console.log('Successfully tweeted with image:', tweet.text);
+    return message;
+
+  } catch (error) {
+    console.error('Error in gatherAndDisplayChainStatsWithImage:', error);
+    return null;
+  }
+}
+
+async function gatherAndDisplayChainStats() {
+  const chains = [process.env.DKG_DB, process.env.OTP_DB, process.env.GNO_DB, process.env.BASE_DB];
+  const stats = {};
+
+  for (const dbName of chains) {
+      if (!dbName) {
+          console.log(`Environment variable for chain ${dbName} not set.`);
+          continue;
+      }
+
+      try {
+          await switchDatabase(dbName);
+
+          const pubsStats = await query('SELECT * FROM v_pubs_stats_last24h;');
+          const nodeStats = await query(`
+              SELECT 
+                  ROUND(AVG(nodeAsk), 3) AS avgNodeAsk, 
+                  SUM(nodeStake) AS totalNodeStake, 
+                  COUNT(*) AS totalNodes 
+              FROM v_nodes 
+              WHERE nodeStake >= 50000;
+          `);
+          const totalTracSpentStats = await query('SELECT SUM(totalTracSpent) as totalTracSpent FROM v_pubs_stats_total;');
+          const totalPubsStats = await query('SELECT totalPubs FROM v_pubs_stats_total;');
+
+          stats[dbName] = {
+              pubs24h: pubsStats[0],
+              nodes: nodeStats[0],
+              totalPubs: totalPubsStats[0],
+              totalTracSpent: totalTracSpentStats[0].totalTracSpent
+          };
+
+      } catch (error) {
+          console.error(`Failed to fetch stats for database ${dbName}:`, error);
+      }
+  }
+
+  const formatNumber = (number) => {
+    if (number >= 1_000_000) {
+        const value = number / 1_000_000;
+        return value % 1 === 0 ? `${value}M` : `${value.toFixed(0)}M`;
+    } else if (number >= 1_000) {
+        const value = number / 1_000;
+        return value % 1 === 0 ? `${value}K` : `${value.toFixed(1)}K`;
+    } else if (number > 1) {
+        return number.toFixed(0);
+    } else {
+        return number.toFixed(2);
+    }
+};
+
+  const formatCurrency = (number) => {
+      if (number >= 1_000_000) {
+          const value = number / 1_000_000;
+          return value % 1 === 0 ? `${value}M` : `${value.toFixed(1)}M`;
+      } else if (number >= 1_000) {
+          const value = number / 1_000;
+          return value % 1 === 0 ? `${value}K` : `${value.toFixed(1)}K`;
+      } else {
+          return number.toFixed(1);
+      }
+  };
+
+      const symbol = 'TRAC';
+      const price = await getCoinPrice(symbol);
+      const marketCap = await getCoinCap(symbol);
+      const volume = await getCoinVolume(symbol);
+      const marketCapFormatted = formatCurrency(marketCap);
+      const volumeFormatted = formatCurrency(volume);
+      const networkStats = await postNetworkStatistics();
+      const tracDailyValue = networkStats.dailyTracSpent;
+      const avgEpochsNumber = networkStats.avgEpochsNumber;
+      const tracYearlyValue = formatNumber(tracDailyValue * 365);
+      const tracDailyUsdValue = formatCurrency(price * tracDailyValue);
+      const tracYearlyUsdValue = formatCurrency(price * tracDailyValue * 365);
+      
+  let message = "== Daily $TRAC Overview 📊 ==\n\n";
+
+  for (const [dbName, chainStats] of Object.entries(stats)) {
+      const { pubs24h, nodes, totalPubs, totalTracSpent  } = chainStats;
+      const chainDisplayName = chainNameMap[dbName] || dbName; // Use dbName if no mapping exists
+      const price = await getCoinPrice('TRAC');
+      const tvlUsdFormatted = formatCurrency(price * nodes.totalNodeStake);
+      const tvlFormatted = formatNumber(nodes.totalNodeStake);
+      
+      message += `${chainDisplayName}\n`;
+      message += `⚙ Nodes: ${nodes.totalNodes}\n`;
+      message += `🔒 TVL: ${tvlFormatted} TRAC ($${tvlUsdFormatted})\n`;
+      message += `💎 Assets (24H/all): ${formatNumber(pubs24h.totalPubs)} / ${formatNumber(totalPubs.totalPubs)}\n`;
+      message += `💰 TRAC spent (24H/all): ${formatNumber(pubs24h.totalTracSpent)} / ${formatNumber(totalTracSpent)}\n\n`;
+    }
+     
+    message += `🪙 Price: $${price} | Volume: $${volumeFormatted}\n`,
+    message += `⚖️ Total Supply: 500M | Mcap: $${marketCapFormatted}\n`,
+    message += `\n📈 In the last 24H, ${formatNumber(tracDailyValue)} $TRAC ($${tracDailyUsdValue}), equivalent to ${tracYearlyValue} TRAC ($${tracYearlyUsdValue}) yearly, was locked for utility for an average of ${(avgEpochsNumber * 90).toFixed(0)} days!\n`;
+    message += "\n👉 Brought to you by OTHub.io 👈";
+    
+  return message;
+}
+
+// TEST FUNCTIONS ABOVE
+// gatherAndDisplayChainStats().then(msg => console.log(msg)).catch(err => console.error('Error:', err));
+
+// const ctx = { reply: (message) => console.log(message) }; // Mock a context for testing
+// fetchAndSendRecordStats(ctx).then(() => console.log('Test completed')).catch(err => console.error('Error:', err));
+
+// fetchNetworkStatistics().then(stats => {
+//   console.log(stats);
+// }).catch(err => console.error('Error:', err));
+
+// const periods = ['daily', 'weekly', 'monthly'];
+// for (let period of periods) {
+//     fetchPeriodStats(period).then(stats => console.log(`${period} stats:`, stats)).catch(err => console.error('Error:', err));
+// }
+
+// postStatistics('daily').then(message => console.log(message)).catch(err => console.error('Error:', err));
+
+// postNetworkStatistics().then(data => {
+//   console.log('Network Stats:', data);
+// }).catch(err => console.error('Error:', err));
+
   module.exports = {
+    gatherAndDisplayChainStats,
+    gatherAndDisplayChainStatsWithImage,
     postTweet,
     postStatistics,
     fetchAndSendRecordStats,
